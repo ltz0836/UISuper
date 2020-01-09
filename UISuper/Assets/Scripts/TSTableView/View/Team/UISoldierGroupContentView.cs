f290cc947ea1fa8d4afb86c97a59dddc58b28602
saeipi <ksert@163.com>
2020-01-09 12:22:23 +0800

    武则天列表

---
+++ UISuper/Assets/Scripts/TSTableView/View/Team/UISoldierGroupContentView.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISoldierGroupContentView : MonoBehaviour
+{
+    public GameObject soldierView;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i= 0; i < 5; i++)
+        {
+            GameObject item = Instantiate(soldierView);
+            item.transform.parent = this.transform;
+            item.transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
